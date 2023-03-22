fd3bf9d5fba74997851aa1fd3266a60b4ad8b617
StephenCao422 <113153218+StephenCao422@users.noreply.github.com>
2023-03-22 14:56:32 -0500

    Add files via upload

---
+++ StartCode/src/sum.cpp
@@ -0,0 +1,23 @@
+#include <chrono>
+#include <cstdio>
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+  // Record the time at the beginning of our experiment.
+  auto begin = std::chrono::steady_clock::now();
+
+  // Experiment takes place here.
+  // This for loop is just a placeholder.
+  for (size_t i = 0; i < 1000000; i++) {
+  }
+
+  // Record the time at the end of our experiment.
+  auto end = std::chrono::steady_clock::now();
+
+  // Compute the duration of the experiment in seconds.
+  double duration = std::chrono::duration<double>(end - begin).count();
+
+  printf("Runtime was %lf us (microseconds).\n", 1e6 * duration);
+
+  return 0;
+}
